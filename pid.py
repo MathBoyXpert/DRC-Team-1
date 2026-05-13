061555f8fe4db648fa98d779068497d547071129
MathBoyXpert <nicholassantoso989@gmail.com>
2026-05-13 14:23:14 +1000

    Sorted into classes and set the variables

--- pid.py
+++ pid.py
@@ -5,28 +5,41 @@ class PID:
     integral = 0
     time_prev = -1e-6
     e_prev = 0
+    Ki = 0
+    Kd = 0
+    Kp = 0
+    setpoint = 0
 
+    def __init__(self):
+        self.setpoint = 0
+
+    ## The selfpoint changes during turns
+    def set_setpoint(self, setpoint):
+        self.setpoint = setpoint
+
+    ## 
     def pid(Kp, Ki, Kd, setpoint, measurement):
         global time, integral, time_prev, e_prev
 
-        offset = 320
-
+        ### The error is the current point and the where you want to go
         e = setpoint - measurement
 
         # PID calculations
         ## Note: We need to tune these Parameters during testing
         ## Start by tuning P first and then tuning I and D afterwards
-        prop = Kp*e
+        prop = Kp * e
         integral = integral + Ki*e*(time - time_prev)
         der = Kd*(e - e_prev)/(time - time_prev)
 
-        MV = offset + prop + integral + der
+        # calculate manipulated variable - MV 
+        manipulated_var = prop + integral + der
 
         e_prev = e
         time_prev = time
 
-        return MV
+        return manipulated_var
     
 
 if __name__=="__main__":
     x = PID()
+    print(x.pid(0.1, 0.1, 0.1, 0.3))

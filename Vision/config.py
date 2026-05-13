bd492b90483a693ae3faa8dae31d7e9c90673dc3
MathBoyXpert <nicholassantoso989@gmail.com>
2026-05-13 13:26:12 +1000

    Testing my own version of preprocessing

--- Vision/config.py
+++ Vision/config.py
@@ -1,6 +1,6 @@
 import cv2
 
 # this is for changing the video input (0..*) with the number
-VIDEO_INPUT = 0
+VIDEO_INPUT = 1
 # this is to indivcate the HSV space, to save on processing power utilise COLOR_BGR2HSV instead of COLOR_BGR2HSV_FULL
 HSV_SPACE = cv2.COLOR_BGR2HSV_FULL

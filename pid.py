c120a43c15db4ee764ccbcb0fc1cdb628c62b94a
MathBoyXpert <nicholassantoso989@gmail.com>
2026-05-13 13:39:52 +1000

    Setting pid functions into a class

--- pid.py
+++ pid.py
@@ -1,29 +1,32 @@
 #!/usr/bin/env python
 
-time = 0
-integral = 0
-time_prev = -1e-6
-e_prev = 0
+class PID:
+    time = 0
+    integral = 0
+    time_prev = -1e-6
+    e_prev = 0
 
-def pid(Kp, Ki, Kd, setpoint, measurement):
-    global time, integral, time_prev, e_prev
+    def pid(Kp, Ki, Kd, setpoint, measurement):
+        global time, integral, time_prev, e_prev
 
-    offset = 320
+        offset = 320
 
-    e = setpoint - measurement
+        e = setpoint - measurement
 
-    # PID calculations
-    ## Note: We need to tune these Parameters during testing
-    ## Start by tuning P first and then tuning I and D afterwards
-    prop = Kp*e
-    integral = integral + Ki*e*(time - time_prev)
-    der = Kd*(e - e_prev)/(time - time_prev)
+        # PID calculations
+        ## Note: We need to tune these Parameters during testing
+        ## Start by tuning P first and then tuning I and D afterwards
+        prop = Kp*e
+        integral = integral + Ki*e*(time - time_prev)
+        der = Kd*(e - e_prev)/(time - time_prev)
 
-    MV = offset + prop + integral + der
+        MV = offset + prop + integral + der
 
-    e_prev = e
-    time_prev = time
+        e_prev = e
+        time_prev = time
 
-    return MV
+        return MV
+    
+
 if __name__=="__main__":
-    pid()
+    x = PID()

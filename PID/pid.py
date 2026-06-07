ee66564e5fefa881d9e1a2e51b00935f4bcab752
Gavin Tchong <z5480803@ad.unsw.edu.au>
2026-06-07 21:20:04 +1000

    Base PID Loop set up

---
+++ PID/pid.py
@@ -0,0 +1,49 @@
+
+class PID:    
+
+    def __init__(self, kp, ki, kd):
+
+        # Changable Coefficients - Should be set by config then be changeable
+        self.kp = float(kp)
+        self.ki = float(ki)
+        self.kd = float(kd)
+
+        # Initialising Vars of New Controller
+        self.target = 0         # What the controller wants the end to be at
+        self.integral = 0       # Integral for the small error correction - I
+        self.prev_error = 0     # Previous error - D 
+
+
+    def set_desired(self, target,):
+        self.target = target
+
+    def compute(self, curr, dt):    
+        # curr is the current angle the drc is facing
+        # dt is delta time, so the difference in time between this cycle and prev 
+
+        error = self.target - curr
+
+        # P calculations
+        P = self.kp * error
+
+        # I calculations
+        self.integral += error * dt
+        I = self.ki * self.integral
+
+        # D calculations 
+        derivative = (error - self.prev_error) / dt
+        D = self.kd * derivative
+
+        output = P + I + D
+        self.prev_error = error
+
+        return output
+    
+
+    ### CONVERSIONS FOR ALL THE RANDOM SHI ###
+
+    
+
+
+    
+

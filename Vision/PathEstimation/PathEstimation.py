11093143eb62b8a8d75e555cfd5fecc0130a39eb
MathBoyXpert <nicholassantoso989@gmail.com>
2026-06-16 01:05:38 +1000

    Added edge case detection

--- Vision/PathEstimation/PathEstimation.py
+++ Vision/PathEstimation/PathEstimation.py
@@ -17,25 +17,45 @@ class PathEstimation:
         ## This grabs the polynomial coefficients that were obtained from the fit
         left_fit = self.yellow_filter.fit
         right_fit = self.blue_filter.fit
-        if left_fit is None or right_fit is None:
+
+        left_curvature = None
+        right_curvature = None
+
+        ## Calculates the theoretical curvature of the left and right curves
+        if left_fit is not None:
+            left_curvature = ((1 + (2* left_fit[0]*y_eval+ left_fit[1])**2)**1.5) / np.abs(2*left_fit[0])
+
+        if right_fit is not None:
+            right_curvature = ((1 + (2 *right_fit[0]*y_eval + right_fit[1])**2)**1.5) / np.abs(2*right_fit[0])
+
+        ## Edge cases: When either onr of the lanes are not in view, return the other lane.
+        if left_curvature is not None and right_curvature is not None:
+            return (left_curvature + right_curvature) / 2
+        elif left_curvature is not None:
+            return left_curvature
+        elif right_curvature is not None:
+            return right_curvature
+        else:
             return None
         
         ## Calculates the theoretical curvature of the left and right curves
-        left_curvature = ((1 + (2* left_fit[0]*y_eval+ left_fit[1])**2)**1.5) / np.abs(2*left_fit[0])
-        right_curvature = ((1 + (2 *right_fit[0]*y_eval + right_fit[1])**2)**1.5) / np.abs(2*right_fit[0])
+        # left_curvature = ((1 + (2* left_fit[0]*y_eval+ left_fit[1])**2)**1.5) / np.abs(2*left_fit[0])
+        # right_curvature = ((1 + (2 *right_fit[0]*y_eval + right_fit[1])**2)**1.5) / np.abs(2*right_fit[0])
 
+        ## Edge Case: U-turn lane. You cannot see one of the lanes
+    
         ## Returns the curve this ill give you the coefficients of the poolynomail which then can be accessed by using polyfit.
-        return (left_curvature + right_curvature) / 2
     
     def calculate_centreline(self):
 
         ### This gets the x_base that was from the histogram calculations
         left_base = self.yellow_filter.x_base
         right_base = self.blue_filter.x_base
-        if left_base is None or right_base is None:
+        if left_base is None and right_base is None:
             return None
         
-
+        ## Edge case: U-turn
+       
         ## This retuns the centreline. 
         return (left_base + right_base) / 2
         

8d273e1e9ca68540e228243cc410573715c66ac9
Chiz3l <anshg345@gmail.com>
2026-06-24 13:41:41 +1000

    updates

--- Vision/HSVFilters/HSVFilterInterface.py
+++ Vision/HSVFilters/HSVFilterInterface.py
@@ -105,7 +105,7 @@ class HSVFilterInterface(ABC):
     def HSV_GUI_Adjustments(self):
         # trackbar window constant is the name of the winodw, window normal allows it to be resized
         cv2.namedWindow(self.Get_Filter_GUI_Name(), cv2.WINDOW_NORMAL)
-        cv2.resizeWindow(self.Get_Filter_GUI_Name(), 350, 700)
+        cv2.resizeWindow(self.Get_Filter_GUI_Name(), 600, 700)
 
         # basic function for the trackbar call back, not needed, hence do nothing
         def doNothing(self):
